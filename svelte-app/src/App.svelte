00a0a79c32c4ded1cae87ab267c58472bfdbf3ed
unknown <e.s.baccio@gmail.com>
2025-10-01 02:02:25 -0600

    modified pocketbase endpoint

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -13,7 +13,8 @@
     import { fetchPeople, fetchPlays, fetchGroups } from './lib/services/DatabaseService';
     
     // Leer variable Vite; usar fallback local para desarrollo.
-    const VITE_POCKETBASE_URL = "pocketbase-production-f5d2.up.railway.app"
+    const VITE_POCKETBASE_URL = "https://pocketbase-production-f5d2.up.railway.app"
+    console.log("Connecting to pocketbase at:", VITE_POCKETBASE_URL)
     const pb = new PocketBase(VITE_POCKETBASE_URL);
     const thumbnailService = new ThumbnailService();
     
