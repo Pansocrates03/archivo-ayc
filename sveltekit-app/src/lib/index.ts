502129322623f6f72321a28bb52ca0b9d17a34d8
unknown <e.s.baccio@gmail.com>
2025-11-17 14:37:28 -0600

    añadido nuevo url con https

--- sveltekit-app/src/lib/index.ts
+++ sveltekit-app/src/lib/index.ts
@@ -1,3 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-export const PB_URL = 'pocketbase-production-4f9d.up.railway.app';
+export const PB_URL = 'https://pocketbase-production-4f9d.up.railway.app';
